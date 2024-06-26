9a0cfd5634d24bd813a122c2898f9c045987d92e
Benji Grant <benjamin.grantGRA0007@gmail.com>
2024-06-27 00:06:45 +1000

    Update log icon

--- src/res/LogIcon.tsx
+++ src/res/LogIcon.tsx
@@ -1,29 +1,14 @@
+import { createLucideIcon } from 'lucide-react'
+
 // TODO: https://github.com/lucide-icons/lucide/issues/2214
-export const LogIcon = ({ className }: { className?: string }) => (
-  // biome-ignore lint/a11y/noSvgWithoutTitle: icon
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className={className}
-  >
-    <path d="M 14 14.5 L 20 14.5" />
-    <path d="M 14 19.5 L 20 19.5" />
-    <path d="M 14 4.5 L 20 4.5" />
-    <path d="M 14 9.5 L 20 9.5" />
-    <path d="M 5 14.5 L 4 14.5" />
-    <path d="M 5 19.5 L 4 19.5" />
-    <path d="M 5 4.5 L 4 4.5" />
-    <path d="M 5 9.5 L 4 9.5" />
-    <path d="M 9 14.5 L 10 14.5" />
-    <path d="M 9 19.5 L 10 19.5" />
-    <path d="M 9 4.5 L 10 4.5" />
-    <path d="M 9 9.5 L 10 9.5" />
-  </svg>
-)
+export const LogIcon = createLucideIcon('TextLog', [
+  ['path', { d: 'M13 12h8' }],
+  ['path', { d: 'M13 18h8' }],
+  ['path', { d: 'M13 6h8' }],
+  ['path', { d: 'M3 12h1' }],
+  ['path', { d: 'M3 18h1' }],
+  ['path', { d: 'M3 6h1' }],
+  ['path', { d: 'M8 12h1' }],
+  ['path', { d: 'M8 18h1' }],
+  ['path', { d: 'M8 6h1' }],
+])
